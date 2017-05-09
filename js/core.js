518cb458345be11d325cf3bda9c7dc98147728c8
Chinara1214 <you@example.com>
2017-05-09 11:27:41 +0600

    #22 работа над страницей blog-item

--- js/core.js
+++ js/core.js
@@ -23,6 +23,17 @@ $(document).ready(function(){
 		$('.slider-1').slick({
     		arrows: true,
    		});
+
+   		$("nav.menu").on("click","a", function (event) {
+
+         event.preventDefault();   
+
+        var id  = $(this).attr('href'),
+                    top = $(id).offset().top;
+
+        $('body,html').animate({scrollTop: top}, 600);
+
+    });
 });
 
  	
